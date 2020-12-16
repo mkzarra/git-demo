97b4b4ac57bb7704e5ac3c43b6798288e7dd5a04
famousPete <pete.macaluso@generalassemb.ly>
2020-12-16 16:36:42 -0500

    [server] add root route

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+	res.send('hello from the root!')
+})
+
 const Port = process.env.PORT || 7165;
 
 app.listen(Port, () => {
